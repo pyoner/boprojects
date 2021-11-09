df7b32460d73aa282affcd3b32a98259f4515890
Askar Yusupov <devex.soft@gmail.com>
2021-11-09 13:30:52 +0500

    feat: add carbon-components-svelte, yaml import plugin for vite

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,15 @@
 import vercel from '@sveltejs/adapter-vercel';
+import { optimizeImports } from 'carbon-preprocess-svelte';
+import content from '@originjs/vite-plugin-content';
 
+console.log(content.default);
 export default {
+  preprocess: [optimizeImports()],
   kit: {
     adapter: vercel(),
     target: '#svelte',
-  },
+    vite: {
+      plugins: [content.default()]
+    }
+  }
 };
