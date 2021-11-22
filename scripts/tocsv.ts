46a71e0750c32c2cbbedeb976e807c708f1cb486
Askar Yusupov <devex.soft@gmail.com>
2021-11-22 12:00:28 +0500

    feat: formate date YYYY-MM-DD

--- scripts/tocsv.ts
+++ scripts/tocsv.ts
@@ -9,7 +9,16 @@ function main() {
   };
   const data = load(fs.readFileSync(file.yaml, 'utf-8')) as unknown[];
   const csv = parse(data, {
-    transforms: [transforms.flatten({ arrays: true })]
+    transforms: [
+      transforms.flatten({ arrays: true }),
+      (item) => {
+        const d = item.published_at as Date;
+        return {
+          ...item,
+          published_at: `${d.toISOString().split('T')[0]}`
+        };
+      }
+    ]
   });
   fs.writeFileSync(file.csv, csv);
 }
