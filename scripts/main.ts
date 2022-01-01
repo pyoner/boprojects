c8023211304adcd4f8845d6a29cf614784b3d778
Askar Yusupov <devex.soft@gmail.com>
2022-01-01 15:11:15 +0500

    build: convert to module

---
+++ scripts/main.ts
@@ -0,0 +1,19 @@
+import { program } from 'bandersnatch';
+
+// import commands
+import { tocsv } from './tocsv';
+import { scrape } from './scrape';
+
+function main() {
+  program()
+    .add(tocsv)
+    .add(scrape)
+    .run()
+    .catch((err) => {
+      console.error(
+        `There was a problem running this command:\n${String(err)}`
+      );
+      process.exit(1);
+    });
+}
+main();
